41a70ece5e2b36e68b028f8dce0c995dab65d098
Stefan <stscit21@hs-esslingen.de>
2020-03-14 19:44:43 +0100

    Update java doc comment

--- src/main/java/factorial/FactorialCalculator.java
+++ src/main/java/factorial/FactorialCalculator.java
@@ -13,6 +13,7 @@ public interface FactorialCalculator<T extends Number> {
      * @param entry The previously calculated result
      * @param n     The number of the factorial to calculate
      * @return The result of the calculation
+     * @throws IllegalArgumentException if n is negative
      */
     T factorial(Map.Entry<Integer, T> entry, int n);
 }
