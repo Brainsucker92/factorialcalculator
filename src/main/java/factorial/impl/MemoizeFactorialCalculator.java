ed2e16f5081d6a69ef58c243fde8ce7285c2c506
Stefan <stscit21@hs-esslingen.de>
2020-09-01 01:21:04 +0200

    Fix memoization issues

--- src/main/java/factorial/impl/MemoizeFactorialCalculator.java
+++ src/main/java/factorial/impl/MemoizeFactorialCalculator.java
@@ -1,5 +1,6 @@
 package factorial.impl;
 
+import java.util.AbstractMap;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
@@ -8,9 +9,9 @@ import factorial.FactorialCalculator;
 
 public class MemoizeFactorialCalculator<T extends Number> implements FactorialCalculator<T> {
 
-    private FactorialCalculator<T> calculator;
+    private final FactorialCalculator<T> calculator;
 
-    private TreeMap<Integer, T> factorialBuffer = new TreeMap<>(Comparator.naturalOrder());
+    private final TreeMap<Integer, T> factorialBuffer = new TreeMap<>(Comparator.naturalOrder());
 
     public MemoizeFactorialCalculator(FactorialCalculator<T> calculator) {
         this.calculator = calculator;
@@ -18,44 +19,41 @@ public class MemoizeFactorialCalculator<T extends Number> implements FactorialCa
 
     @Override
     public T factorial(int n) {
-        return calculator.factorial(n);
+        int i = 0;
+        T identity = calculator.factorial(i);
+        Map.Entry<Integer, T> entry = new AbstractMap.SimpleEntry<>(i, identity);
+        return factorial(entry, n);
     }
 
     @Override
     public T factorial(Map.Entry<Integer, T> entry, int n) {
+        if (n < 0) {
+            return calculator.factorial(n);
+        }
+
+        Integer entryKey = entry.getKey();
+        if (!factorialBuffer.containsKey(entryKey)) {
+            T entryValue = entry.getValue();
+            factorialBuffer.put(entryKey, entryValue);
+        }
+
         if (factorialBuffer.containsKey(n)) {
             // Result is already known
             return factorialBuffer.get(n);
         }
 
-        // Check internal buffer first:
-        Map.Entry<Integer, T> floorEntry = factorialBuffer.floorEntry(n);
-        if (floorEntry == null) {
-            // factorialBuffer is empty
-            int i = 1;
-            T identity = calculator.factorial(i);
-            factorialBuffer.put(i, identity);
-        }
-        Integer externalKey = entry.getKey();
-
-        // Compare keys and decide which entry to use:
-        Map.Entry<Integer, T> closestEntry;
         do {
             // Find current floorEntry:
-            floorEntry = factorialBuffer.floorEntry(n);
+            Map.Entry<Integer, T> floorEntry = factorialBuffer.floorEntry(n);
             Integer floorEntryKey = floorEntry.getKey();
 
-            // Determine closest entry:
-            int diff = floorEntryKey.compareTo(externalKey);
-            closestEntry = (diff > 0) ? floorEntry : entry;
-
             // Calculate new entry:
-            int newKey = closestEntry.getKey() + 1;
-            T newValue = calculator.factorial(closestEntry, newKey);
+            int newKey = floorEntryKey + 1;
+            T newValue = calculator.factorial(floorEntry, newKey);
 
             // Update current data:
             factorialBuffer.put(newKey, newValue);
-        } while (closestEntry.getKey() < n);
+        } while (factorialBuffer.floorEntry(n).getKey() < n);
 
         return factorialBuffer.get(n);
     }
